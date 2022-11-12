fab233a66356087f007efc9f3b6966b56f2c56e0
t-bowcock <tyler.j.bowock@gmail.com>
2022-11-12 17:49:53 +0000

    trying to fix angular and django

--- fyp/fyp/settings.py
+++ fyp/fyp/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "fyp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "fyp/templates"],
+        "DIRS": [BASE_DIR / "fyp/static"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
