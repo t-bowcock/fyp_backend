2fe614f994762103d981eaefb9dc3a9dfb71bcde
t-bowcock <tyler.j.bowcock@gmail.com>
2022-11-15 18:13:35 +0000

    pulls data from auraDB instance and sort of responds to the front end

--- DjangoAPI/DjangoAPI/settings.py
+++ DjangoAPI/DjangoAPI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
+    "django_neomodel",
     "QueryApp.apps.QueryappConfig",
     "ManagementApp.apps.ManagementappConfig",
     "DemoApp.apps.DemoappConfig",
